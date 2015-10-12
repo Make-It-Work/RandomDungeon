76e495567c558c8a19bffabe43b63b48ed70c0a3
Make-It-Work <swhjenni@student.avans.nl>
2015-10-12 14:41:25 +0200

    Memory Leak spullen

--- RandomDungeon/RandomDungeon/RandomDungeon.cpp
+++ RandomDungeon/RandomDungeon/RandomDungeon.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
     return 0;
 }
 
